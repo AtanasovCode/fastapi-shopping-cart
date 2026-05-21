d2c66c43d9644cdc8fe9dbcea01b9b68688d699f
AtanasovCode <a_ljupce@yahoo.com>
2026-05-21 13:56:30 +0200

    create new user & cart sqlalchemy models - create new user model - create new cart model - create new cart items model - create relationships between new models

--- model/models.py
+++ model/models.py
@@ -36,3 +36,37 @@ class Product(Base):
 
     category = relationship("Category", back_populates="products")
     manufacturer = relationship("Manufacturer", back_populates="products")
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+    cart = relationship("Cart", back_populates="user")
+
+
+class Cart(Base):
+    __tablename__ = "carts"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="cart")
+    items = relationship("CartItems", back_populates="cart")
+
+
+class CartItems(Base):
+    __tablename__ = "cart_items"
+
+    id = Column(Integer, primary_key=True)
+    cart_id = Column(Integer, ForeignKey("carts.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer)
+
+    cart = relationship("Cart", back_populates="items")
+    product = relationship("Product")
